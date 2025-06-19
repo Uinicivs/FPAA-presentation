624c5053b5dc5cc3c2bf243330251e2f00a3d88e
Uinicivs <viniciushqalves@gmail.com>
2025-06-19 16:26:43 -0300

    fix: it was not working :(

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('express').urlencoded({ extended: false });
 
+const dpController = require("./controllers/dpController");
+const dpbtController = require('./controllers/dpbtController');
+
 const app = express();
 const PORT = 3000;
 
-const lcsController = require('./controllers/lcsController');
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.render('index');
 });
 
-app.post('/result', bodyParser, lcsController.processLCS);
+app.post('/dp', bodyParser, dpController.process);
+app.post('/dpbt', bodyParser, dpbtController.process);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
